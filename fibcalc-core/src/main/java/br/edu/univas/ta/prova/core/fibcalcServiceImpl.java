e9ef225cd4921cdf5a346db53cd36685c9666c55
JeffLePe <jeffersonleon310@yahoo.com.br>
2017-10-02 20:40:17 -0300

    segundo commit

---
+++ fibcalc-core/src/main/java/br/edu/univas/ta/prova/core/fibcalcServiceImpl.java
@@ -0,0 +1,15 @@
+package br.edu.univas.ta.prova.core;
+
+import br.edu.univas.ta.prova.api.Result;
+import br.edu.univas.ta.prova.api.fibcalcService;
+import br.edu.univas.ta.prova.lib.FibonattiCalculation;
+
+public class fibcalcServiceImpl implements fibcalcService{
+
+	@Override
+	public Result fib(int num) {
+		int result = FibonattiCalculation.fibonacci(num);
+		return new Result().withNum(num).withResult(result);
+	}
+
+}
